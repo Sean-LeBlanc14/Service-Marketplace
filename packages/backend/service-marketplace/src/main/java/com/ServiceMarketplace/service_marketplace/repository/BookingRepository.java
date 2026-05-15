adfceb96355ddc6a327abe83dab0f5d33c5d00f7
Sean_LeBlanc <seanaugustlg2006@gmail.com>
2026-05-15 13:03:50 -0700

    feat: Create booking repository

---
+++ packages/backend/service-marketplace/src/main/java/com/ServiceMarketplace/service_marketplace/repository/BookingRepository.java
@@ -0,0 +1,10 @@
+package com.ServiceMarketplace.service_marketplace.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ServiceMarketplace.service_marketplace.model.Booking;
+
+@Repository
+public interface BookingRepository extends MongoRepository<Booking, String> {
+}

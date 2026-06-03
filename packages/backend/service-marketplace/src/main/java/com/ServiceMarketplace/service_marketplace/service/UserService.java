6eeac6e6deb9a70a71a68d3f564e282e53026cf3
Edgar Reynoso <reynosoedgar276@gmail.com>
2026-06-03 12:51:42 -0700

    Fix: new user service

--- packages/backend/service-marketplace/src/main/java/com/ServiceMarketplace/service_marketplace/service/UserService.java
+++ packages/backend/service-marketplace/src/main/java/com/ServiceMarketplace/service_marketplace/service/UserService.java
@@ -19,7 +19,6 @@ import com.ServiceMarketplace.service_marketplace.dto.ServiceDto;
 import com.ServiceMarketplace.service_marketplace.dto.UpdateUserProfileRequest;
 import com.ServiceMarketplace.service_marketplace.dto.UserProfile;
 import com.ServiceMarketplace.service_marketplace.exception.EmailAlreadyExistsException;
-import com.ServiceMarketplace.service_marketplace.exception.InvalidEmailDomainException;
 import com.ServiceMarketplace.service_marketplace.exception.ResourceNotFoundException;
 import com.ServiceMarketplace.service_marketplace.model.User;
 import com.ServiceMarketplace.service_marketplace.repository.UserRepository;
@@ -54,10 +53,6 @@ public class UserService {
     }
 
     public AuthResponse registerUser(RegisterRequest request) {
-        if (!request.getEmail().endsWith("@calpoly.edu")) {
-            throw new InvalidEmailDomainException("Registration is limited to Cal Poly email addresses.");
-        }
-
         if (userRepository.existsByEmail(request.getEmail())) {
             throw new EmailAlreadyExistsException(request.getEmail());
         }
@@ -112,12 +107,6 @@ public class UserService {
         
     }
     
-    //Temp function -> Need it for booking first name + last name -> we can add these fields to the booking response in the future if needed
-    public UserProfile getUserById(String id){
-        var user = userRepository.findById(id).orElseThrow(() -> new UsernameNotFoundException("User not found"));
-
-        return toUserProfile(user);
-    }
 
     public UserProfile updateUserProfile(UserDetails userDetails, UpdateUserProfileRequest request){
 

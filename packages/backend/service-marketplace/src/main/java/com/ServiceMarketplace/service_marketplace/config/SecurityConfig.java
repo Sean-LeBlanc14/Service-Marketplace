18457bef0aea0ebfb9d498bdd54d2874b940ad39
Sean_LeBlanc <seanaugustlg2006@gmail.com>
2026-06-05 02:12:02 -0700

    Added prod url to allowed origins

--- packages/backend/service-marketplace/src/main/java/com/ServiceMarketplace/service_marketplace/config/SecurityConfig.java
+++ packages/backend/service-marketplace/src/main/java/com/ServiceMarketplace/service_marketplace/config/SecurityConfig.java
@@ -47,7 +47,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://localhost:5174"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://localhost:5174", "https://service-marketplace-silk.vercel.app"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
